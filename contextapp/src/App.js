c43ca5b6231de6854a56c79b4ea3d38e46311d8e
Your Name <you@example.com>
2021-08-27 16:28:48 +0200

    react cpntext API

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
+import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
 
+import ThemeContextProvider, { ThemeContext } from './contexts/ThemeContext';
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+
+      <AuthContextProvider>
+        <Navbar />
+        <BookList />
+        <ThemeToggle />
+      </AuthContextProvider>
+        
+      </ThemeContextProvider>
     </div>
   );
 }
